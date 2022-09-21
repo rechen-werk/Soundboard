fe7b958d524379bc74de0a47c038e20cce72a9e3
rechenwerk-a <adrian.vinojcic@gmail.com>
2022-09-21 21:35:48 +0200

    Moved main to other class so that I can put dependencies in the .jar, because javafx is dogshit

---
+++ src/main/java/eu/rechenwerk/soundboard/FXSoundBoard.java
@@ -0,0 +1,25 @@
+package eu.rechenwerk.soundboard;
+
+import eu.rechenwerk.soundboard.controller.SoundBoardController;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+public class FXSoundBoard extends Application {
+	@Override
+	public void start(Stage stage) throws IOException, URISyntaxException {
+		FXMLLoader fxmlLoader = new FXMLLoader(FXSoundBoard.class.getResource("soundboard-view.fxml"));
+		Scene scene = new Scene(fxmlLoader.load(), 1000, 600);
+		stage.setTitle("Soundboard");
+		stage.setScene(scene);
+
+		SoundBoardController controller = fxmlLoader.getController();
+		controller.init(stage);
+
+		stage.show();
+	}
+}
--- src/main/java/eu/rechenwerk/soundboard/SoundBoard.java
+++ src/main/java/eu/rechenwerk/soundboard/SoundBoard.java
@@ -1,28 +1,7 @@
 package eu.rechenwerk.soundboard;
 
-import eu.rechenwerk.soundboard.controller.SoundBoardController;
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
-import java.io.IOException;
-
-public class SoundBoard extends Application {
-	@Override
-	public void start(Stage stage) throws IOException {
-		FXMLLoader fxmlLoader = new FXMLLoader(SoundBoard.class.getResource("soundboard-view.fxml"));
-		Scene scene = new Scene(fxmlLoader.load(), 1000, 600);
-		stage.setTitle("Soundboard");
-		stage.setScene(scene);
-
-		SoundBoardController controller = fxmlLoader.getController();
-		controller.init(stage);
-
-		stage.show();
-	}
-
+public class SoundBoard {
 	public static void main(String[] args) {
-		launch();
+		FXSoundBoard.launch();
 	}
 }
