f629628a15d78d2991c708246a35aadf78e2fa61
rechenwerk-a <adrian.vinojcic@gmail.com>
2022-09-21 21:28:50 +0200

    Commented out controls dependency

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,16 +1,17 @@
 module eu.rechenwerk.soundboard.soundboard {
-	requires javafx.controls;
+	//requires javafx.controls;
 	requires javafx.fxml;
 	requires javafx.web;
 	requires commons.exec;
 	requires org.json;
 	requires java.desktop;
 
-	opens eu.rechenwerk.soundboard to javafx.fxml;
 	exports eu.rechenwerk.soundboard;
 	exports eu.rechenwerk.soundboard.controller;
-	opens eu.rechenwerk.soundboard.controller to javafx.fxml;
 	exports eu.rechenwerk.soundboard.model.exceptions;
 	exports eu.rechenwerk.soundboard.framework;
-	opens eu.rechenwerk.soundboard.framework to javafx.fxml;
+
+	opens eu.rechenwerk.soundboard;
+	opens eu.rechenwerk.soundboard.controller;
+	opens eu.rechenwerk.soundboard.framework;
 }
