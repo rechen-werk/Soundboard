fb59436e8a9d13084819691533027dad16dc0ffe
rechenwerk-a <adrian.vinojcic@gmail.com>
2022-10-01 19:32:46 +0200

    Added GUI to exception-popup

--- src/main/java/eu/rechenwerk/soundboard/controller/ExceptionPopupController.java
+++ src/main/java/eu/rechenwerk/soundboard/controller/ExceptionPopupController.java
@@ -1,3 +1,14 @@
 package eu.rechenwerk.soundboard.controller;
 
-public class ExceptionPopupController {}
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+
+public class ExceptionPopupController {
+	@FXML private CheckBox askAgainCheckbox;
+	public void onReportErrorClick(ActionEvent actionEvent) {
+	}
+
+	public void onOpenLogClick(ActionEvent actionEvent) {
+	}
+}
