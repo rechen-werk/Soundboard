30ae00333829288c2cf7641a41a2b210267acd23
rechenwerk-a <adrian.vinojcic@gmail.com>
2022-10-16 18:50:17 +0200

    added labels to sound panels

--- src/main/java/eu/rechenwerk/soundboard/view/SoundPane.java
+++ src/main/java/eu/rechenwerk/soundboard/view/SoundPane.java
@@ -4,20 +4,34 @@ import eu.rechenwerk.soundboard.model.images.GradientGenerator;
 
 import eu.rechenwerk.soundboard.model.microphone.VirtualMicrophone;
 import eu.rechenwerk.soundboard.model.sounds.Sound;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.layout.*;
 
 
-import java.awt.*;
+import java.awt.Color;
 import java.util.List;
 
 public class SoundPane extends Pane {
 	private final VirtualMicrophone selectedMicrophone;
+
+
 	public SoundPane(Color tl, Color bl, Color tr, Color br, Sound sound, List<VirtualMicrophone> microphones) {
 		selectedMicrophone = microphones.get(0);
-		Image image = sound == null
-			? GradientGenerator.generateImage(500,500, tl, bl, tr, br)
-			: GradientGenerator.generateImage("speaker.png", tl, bl, tr, br);
+		Image image;
+		if(sound == null) {
+			image = GradientGenerator.generateImage(500,500, tl, bl, tr, br);
+		} else {
+			getChildren().addAll(
+				new VBox(
+					new Label(sound.getName()),
+					new Label(String.format("%.2f", sound.getDuration()))
+				)
+			);
+			image = GradientGenerator.generateImage("speaker.png", tl, bl, tr, br);
+			setOnMouseClicked(event -> sound.play(selectedMicrophone));
+			hoverProperty().addListener((obs, o, n) -> System.out.println(sound.getName()));
+		}
 
 		setBackground(new Background(new BackgroundImage(
 			image,
@@ -26,15 +40,8 @@ public class SoundPane extends Pane {
 			BackgroundPosition.DEFAULT,
 			new BackgroundSize(1.0, 1.0, true, true, false, false)
 		)));
-		setOnMouseClicked(event -> {
-			if(sound != null) {
-				sound.play(selectedMicrophone);
-			}
-		});
-		this.hoverProperty().addListener((obs, o, n) -> {
-			if (sound != null) {
-				System.out.println(sound.getName());
-			}
-		});
+
+
+
 	}
 }
